93f683320bed6886d100a3e6e6358af3606ff540
Atharva Kale <112189075+KaleAtharva@users.noreply.github.com>
2023-08-27 15:10:20 +0530

    Add files via upload

---
+++ Text Editor/learn React/Header.js
@@ -0,0 +1,14 @@
+export function Header(){
+    return(
+        <header>
+            <nav className="nav">
+                <img src="rH6yDfWyJ.png" className="image"/>
+                <ul className="nav-items">
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
